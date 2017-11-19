611707114cb176195679435b8987aa29cfda5092
mrjasonweaver <indyplanets@gmail.com>
2017-11-18 20:48:46 -0600

    chore: clean up and code comments

--- src/app/entry.service.ts
+++ src/app/entry.service.ts
@@ -7,10 +7,13 @@ export class EntryService {
   private root = 'https://jsonplaceholder.typicode.com/posts';
   
     constructor(private http: Http) { }
-  
+
     async getEntries(): Promise<Array<any>> {
       const response = await this.http.get(this.root).toPromise();
+      // grab all 100 posts
       return response.json();
+      // there's no pagination endpoints
+      // so we'll filter them on the front end
     }
 
 }
--- src/app/search-entries/search-entries.component.ts
+++ src/app/search-entries/search-entries.component.ts
@@ -19,43 +19,56 @@ export class SearchEntriesComponent implements OnInit {
   submitted: boolean = false;
 
   constructor(private entryService: EntryService) { }
-  
+
   async ngOnInit() {
+
+    // store the entire payload so we can filter it
     this.entries = await this.entryService.getEntries();
+
+    // set everything up with pagination
+    // 10 posts per page
     this.filteredEntries = this.entries.filter((e, i) => i < 10);
     this.totalItems = this.entries.length;
-    console.log(this.selected);
   }
 
-  filterEntries() {
-    this.filteredEntries = this.entries.filter(e => {
-      return e.title.includes(this.selected) || e.body.includes(this.selected);
-    });
-    this.totalItems = this.filteredEntries.length;
-  }
-
+  // searching on form submit
   searchEntries(event: any) {
     event.preventDefault();
-    if (this.selected !== '' || this.selected !== undefined) {
-      this.filterEntries();
-    } else {
-      this.filteredEntries = this.entries;
-    }
+
+    // flag for frontend conditionals
     this.submitted = true;
+
+    // return first 10 entires if input is reset
+    if (this.selected === '' || this.selected === undefined) {
+      return this.filteredEntries = this.entries.filter((e, i) => i < 10);
+    
+    // return entires filtered based on string submitted
+    } else {
+      this.filteredEntries = this.entries.filter(e => {
+        return e.title.includes(this.selected) || e.body.includes(this.selected);
+      });
+    }
   }
 
+  // handle pagination
   pageChanged(event: any) {
+
+    // do some math based on user input
     this.start = parseInt((event.page - 1) + "0");
     this.end = event.page * 10;
+
+    // return filtered entries based on page clicked
     this.filteredEntries = this.entries.filter((e, i) => i > (this.start - 1) && i < this.end);
   }
 
+  // clear out the searched strings and reset the page
   clearSearched() {
     this.selected = '';
     this.filteredEntries = this.entries.filter((e, i) => i < 10);
     this.submitted = false;
   }
 
+  // shows and hides the pagination and selected search term
   isSearched(searched) {
     if (searched === undefined || searched === '') {
       return false;
@@ -64,6 +77,7 @@ export class SearchEntriesComponent implements OnInit {
     }
   }
 
+  // shows and hides the number of entries found
   showEntryCount(searched) {
     if (this.filteredEntries.length && searched !== undefined && searched !== '' && this.submitted === true) {
       return true;
